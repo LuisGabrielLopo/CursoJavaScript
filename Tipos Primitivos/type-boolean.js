9fb27bc389e1b48b07b2a60a45a5f68712a430cf
Luis <lglopo14@gmail.com>
2022-02-16 14:20:35 -0300

    Exemplos de tipos primitivos

---
+++ Tipos Primitivos/type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+// True -> verdadeiro
+// False -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Curso";
+const texto2 = "curso";
+
+console.log(texto1===texto2)
---
+++ Tipos Primitivos/type-number.js
@@ -0,0 +1,24 @@
+// tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica)
+
+//ponto flutuante
+
+const numerPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+
+const novaOperacao = primeiroNumero / numerPontoFlutuante;
+console.log(novaOperacao);
+
+//NaN -> not a number  ( não é um número)
+
+const curso = "Curso";
+console.log(curso * primeiroNumero)
