a4ba4e1b4a1f6f2723b360c678162f3570f1d917
Luis <lglopo14@gmail.com>
2022-02-16 15:54:38 -0300

    codigos referente aos tipos de variaveis e conversoes

---
+++ TIpos de Variaveis/booleanos.js
@@ -0,0 +1,28 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log("" == false) //String vazia
+console.log(1 == true)
+
+//null ===> vazio
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "curso"
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+//undefined
---
+++ TIpos de Variaveis/conversoes.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos 
+
+// conversão implicita
+const numero = 123;
+const numeroString = "123a";
+
+console.log(numero == numeroString)
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+// conversão explicita
